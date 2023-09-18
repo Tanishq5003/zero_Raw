64fbd494b34048ee612d80d015c090dc62967720
Tanishq <tannu.btp.tk@gmail.com>
2023-09-18 16:05:19 +0530

    commited on 4 pm 18/9/23

--- app/src/main/java/com/example/zeroraw/Details.kt
+++ app/src/main/java/com/example/zeroraw/Details.kt
@@ -1,7 +1,9 @@
 package com.example.zeroraw
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.RadioGroup
@@ -25,8 +27,93 @@ class Details : AppCompatActivity() {
     lateinit var furnished: String
     lateinit var parkingStatus: RadioGroup
     lateinit var parking: String
+    lateinit var otherRooms: ArrayList<String>
+    lateinit var intent1  :Intent
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+        basement_text = findViewById(R.id.basement_text)
+        basement_text.setOnCheckedChangeListener{ group , checkedId ->
+            when(checkedId){
+                R.id.basement_yes ->{
+                    basement = "Yes"
+                }
+                R.id.basement_no ->{
+                    basement = "No"
+                }
+            }
+        }
+        furnishingStatus = findViewById(R.id.furnishing)
+        furnishingStatus.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId){
+                R.id.furnished ->{
+                    furnished = "Yes"
+                }
+                R.id.not_furnished ->{
+                    furnished = "No"
+                }
+            }
+        }
+        parkingStatus = findViewById(R.id.parking)
+        parkingStatus.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId){
+                R.id.parking_yes -> {
+                    parking = "Yes"
+                }
+                R.id.parking_no ->{
+                    parking = "No"
+                }
+            }
+        }
+        totalArea = findViewById(R.id.area_total)
+        carpetArea = findViewById(R.id.area_carpet)
+        buildupArea = findViewById(R.id.area_buildup)
+        totalFloors = findViewById(R.id.floors)
+        pooja = findViewById(R.id.pooja)
+        servant = findViewById(R.id.servant)
+        study = findViewById(R.id.study)
+        other = findViewById(R.id.other)
+        pooja.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked){
+            otherRooms.add("Pooja Room")
+                Log.d("Room Added", "Pooja Room")
+            }
+            else{
+                otherRooms.removeIf { it == "Pooja Room" }
+                Log.d("Room Removed", "Pooja Room")
+                }
+            }
+        servant.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked){
+            otherRooms.add("Servant Room")
+            }
+            else{
+                otherRooms.removeIf { it == "Servant Room" }
+                }
+            }
+        study.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked){
+            otherRooms.add("Study Room")
+            }
+            else{
+                otherRooms.removeIf { it == "Study Room" }
+                }
+            }
+        other.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked){
+            otherRooms.add("Other Rooms")
+            }
+            else{
+                otherRooms.removeIf { it == "Other Room" }
+                }
+            }
+        val address = intent.getStringExtra("Address")
+        val price = intent.getStringExtra("Price")
+        val type = intent.getStringExtra("Property type")
+        val nbed = intent.getStringExtra("nbed")
+        val nbal = intent.getStringExtra("nbal")
+        val nbath = intent.getStringExtra("nbath")
+        val photo1 = intent.getIntExtra("photo1", 0)
+        val photo2 = intent.getIntExtra("photo2", 0)
+        }
     }
-}
